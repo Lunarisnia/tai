6ae81b4ec41f3e83715755d415b3b5e8b9576b82
louna <r.arswendo.r@gmail.com>
2024-06-08 17:38:04 +0700

    safe printing added. real janky

--- src/main.rs
+++ src/main.rs
@@ -8,18 +8,27 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-static HELLO: &[u8] = b"Hello World!";
+static mut CURRENT_OFFSET: isize = 0;
+
+fn safe_print(str: &str) {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    let mut last_index: isize = 0;
+    for (i, &byte) in str.as_bytes().iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(CURRENT_OFFSET + (i as isize * 2)) = byte;
+            *vga_buffer.offset(CURRENT_OFFSET + (i as isize * 2 + 1)) = 0xb;
+        }
+        last_index = i as isize + 1;
+    }
+    unsafe {
+        CURRENT_OFFSET = CURRENT_OFFSET + (last_index * 2);
+    }
+}
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
-
+    safe_print("Fuck Me Man Woah this somehow worked ");
+    safe_print("well enough that I can do this");
     loop {}
 }

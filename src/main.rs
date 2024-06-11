372bb82839f2275dac16e5111e392309fcf2f0cc
louna <r.arswendo.r@gmail.com>
2024-06-12 00:21:30 +0700

    add color code mapping

--- src/main.rs
+++ src/main.rs
@@ -10,14 +10,14 @@ fn panic(_info: &PanicInfo) -> ! {
 
 static mut CURRENT_OFFSET: isize = 0;
 
-fn safe_print(str: &str) {
+fn safe_print(color: ColorCode, str: &str) {
     let vga_buffer = 0xb8000 as *mut u8;
 
     let mut last_index: isize = 0;
     for (i, &byte) in str.as_bytes().iter().enumerate() {
         unsafe {
             *vga_buffer.offset(CURRENT_OFFSET + (i as isize * 2)) = byte;
-            *vga_buffer.offset(CURRENT_OFFSET + (i as isize * 2 + 1)) = 0xb;
+            *vga_buffer.offset(CURRENT_OFFSET + (i as isize * 2 + 1)) = color.0;
         }
         last_index = i as isize + 1;
     }
@@ -26,9 +26,39 @@ fn safe_print(str: &str) {
     }
 }
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u8)]
+enum Color {
+    Black = 0,
+    Blue = 1,
+    Green = 2,
+    Cyan = 3,
+    Red = 4,
+    Magenta = 5,
+    Brown = 6,
+    LightGray = 7,
+    DarkGray = 8,
+    LightBlue = 9,
+    LightGreen = 10,
+    LightCyan = 11,
+    LightRed = 12,
+    Pink = 13,
+    Yellow = 14,
+    White = 15,
+}
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+#[repr(transparent)]
+struct ColorCode(u8);
+
+impl ColorCode {
+    pub fn new(background: Color, foreground: Color) -> ColorCode {
+        return ColorCode((background as u8) << 4 | foreground as u8);
+    }
+}
+
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    safe_print("Fuck Me Man Woah this somehow worked ");
-    safe_print("well enough that I can do this");
+    safe_print(ColorCode::new(Color::Red, Color::Blue), "Hey");
     loop {}
 }
